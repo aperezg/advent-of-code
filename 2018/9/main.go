eeefa0c8ca18918f8f1c2d3b70ec995b49189fa3
aperezg <perezg.adrian@gmail.com>
2018-12-09 12:05:16 +0100

    Calculate movements without 23 rule

---
+++ 2018/9/main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main() {
+	game(9, 25)
+}
+
+const initPos = 1
+
+func game(players, maxMarbles int) (winner int) {
+	board := []int{0}
+	pos := 0
+	p := 1
+
+	for m := 0; m < maxMarbles; m++ {
+		if p == players {
+			p = 1
+		}
+
+		pos, board = move(m+1, pos, board)
+		fmt.Printf("[%d] %v\n", p, board)
+		pos++
+		p++
+	}
+
+	return 0
+}
+
+func move(val, current int, slice []int) (int, []int) {
+	if len(slice) == 1 {
+		slice = append(slice, val)
+		return 1, slice
+	}
+
+	if len(slice) == current {
+		pos := 1
+		return pos, insert(val, 1, 2, slice)
+	}
+
+	nextPos := current + 1
+	return nextPos, insert(val, nextPos, nextPos+1, slice)
+}
+
+func insert(val, start, end int, slice []int) []int {
+	slice = append(slice, 0)
+	copy(slice[end:], slice[start:])
+	slice[start] = val
+
+	return slice
+}
---
+++ 2018/9/main_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func gameTest(t *testing.T) {
+
+}
